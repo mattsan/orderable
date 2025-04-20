8148e409e04259fcd204325fd749097e7443867a
MATSUMOTO Eiji <e.mattsan@gmail.com>
2025-04-20 10:30:54 +0900

    add reorder function

--- lib/orderable.ex
+++ lib/orderable.ex
@@ -1,18 +1,76 @@
 defmodule Orderable do
   @moduledoc """
-  Documentation for `Orderable`.
+  Orderable.
   """
+
+  @type item() :: map() | struct()
 
   @doc """
-  Hello world.
+  Reorder items.
 
-  ## Examples
+  Items must be Map or Struct with ordering key.
 
-      iex> Orderable.hello()
-      :world
+  - `items` - ordered list of items.
+  - `from` - index of item from.
+  - `to` - index of item to.
 
+  ```elixir
+  iex> items = [
+  ...>   %{name: "one", order: 1},
+  ...>   %{name: "two", order: 2},
+  ...>   %{name: "three", order: 3},
+  ...>   %{name: "four", order: 4},
+  ...>   %{name: "five", order: 5}
+  ...> ]
+  iex> Orderable.reorder(items, 3, 1) |> Enum.sort_by(& &1.order)
+  [
+    %{name: "one", order: 1},
+    %{name: "four", order: 2},
+    %{name: "two", order: 3},
+    %{name: "three", order: 4},
+    %{name: "five", order: 5}
+  ]
+  ```
   """
-  def hello do
-    :world
+  @spec reorder([item()], integer(), integer()) :: [item()]
+
+  def reorder(items, from, to) when is_integer(from) and is_integer(to) and to < 0 do
+    reorder(items, from, 0)
+  end
+
+  def reorder(items, from, to) when is_integer(from) and is_integer(to) and from > to do
+    {leading_items, rest} = Enum.split(items, to)
+    {[target_head | _] = target_items, trailing_items} = Enum.split(rest, from - to + 1)
+
+    target_items =
+      target_items
+      |> Enum.chunk_every(2, 1)
+      |> Enum.map(fn
+        [item1, item2] -> update_item(item1, item2.order)
+        [item] -> update_item(item, target_head.order)
+      end)
+
+    leading_items ++ target_items ++ trailing_items
+  end
+
+  def reorder(items, from, to) when is_integer(from) and is_integer(to) and from < to do
+    {leading_items, rest} = Enum.split(items, from)
+    {[target_head | _] = target_items, trailing_items} = Enum.split(rest, to - from + 1)
+
+    target_items =
+      target_items
+      |> Enum.chunk_every(2, 1)
+      |> Enum.map(fn
+        [item1, item2] -> update_item(item2, item1.order)
+        [item] -> update_item(target_head, item.order)
+      end)
+
+    leading_items ++ target_items ++ trailing_items
+  end
+
+  def reorder(items, _, _), do: items
+
+  defp update_item(item, order) when is_map(item) or is_struct(item) do
+    %{item | order: order}
   end
 end
--- test/orderable_test.exs
+++ test/orderable_test.exs
@@ -2,7 +2,369 @@ defmodule OrderableTest do
   use ExUnit.Case
   doctest Orderable
 
-  test "greets the world" do
-    assert Orderable.hello() == :world
+  setup do
+    items = Enum.map(0..4, &%{name: "item-#{&1}", order: &1 * 10})
+
+    [items: items]
+  end
+
+  describe "reorder/3 from 0" do
+    test "move #0 to #-1", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 0, -1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #0 to #0", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 0, 0) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #0 to #1", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 0, 1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #0 to #2", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-0", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 0, 2) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #0 to #3", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-0", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 0, 3) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #0 to #4", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-0", order: 40}
+             ] = Orderable.reorder(items, 0, 4) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #0 to #5", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-0", order: 40}
+             ] = Orderable.reorder(items, 0, 5) |> Enum.sort_by(& &1.order)
+    end
+  end
+
+  describe "reorder/3 from 1" do
+    test "move #1 to #-1", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 1, -1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #1 to #0", %{items: items} do
+      assert [
+               %{name: "item-1", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 1, 0) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #1 to #1", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 1, 1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #1 to #2", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 1, 2) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #1 to #3", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-1", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 1, 3) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #1 to #4", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-1", order: 40}
+             ] = Orderable.reorder(items, 1, 4) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #1 to #5", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-1", order: 40}
+             ] = Orderable.reorder(items, 1, 5) |> Enum.sort_by(& &1.order)
+    end
+  end
+
+  describe "reorder/3 from 2" do
+    test "move #2 to #-1", %{items: items} do
+      assert [
+               %{name: "item-2", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 2, -1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #2 to #0", %{items: items} do
+      assert [
+               %{name: "item-2", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 2, 0) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #2 to #1", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-2", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 2, 1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #2 to #2", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 2, 2) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #2 to #3", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 2, 3) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #2 to #4", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-2", order: 40}
+             ] = Orderable.reorder(items, 2, 4) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #2 to #5", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-2", order: 40}
+             ] = Orderable.reorder(items, 2, 5) |> Enum.sort_by(& &1.order)
+    end
+  end
+
+  describe "reorder/3 from 3" do
+    test "move #3 to #-1", %{items: items} do
+      assert [
+               %{name: "item-3", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 3, -1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #3 to #0", %{items: items} do
+      assert [
+               %{name: "item-3", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 3, 0) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #3 to #1", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-3", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 3, 1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #3 to #2", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-3", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 3, 2) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #3 to #3", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 3, 3) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #3 to #4", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 3, 4) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #3 to #5", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 3, 5) |> Enum.sort_by(& &1.order)
+    end
+  end
+
+  describe "reorder/3 from 4" do
+    test "move #4 to #-1", %{items: items} do
+      assert [
+               %{name: "item-4", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 4, -1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #4 to #0", %{items: items} do
+      assert [
+               %{name: "item-4", order: 0},
+               %{name: "item-0", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 4, 0) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #4 to #1", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-4", order: 10},
+               %{name: "item-1", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 4, 1) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #4 to #2", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-4", order: 20},
+               %{name: "item-2", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 4, 2) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #4 to #3", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-4", order: 30},
+               %{name: "item-3", order: 40}
+             ] = Orderable.reorder(items, 4, 3) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #4 to #4", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 4, 4) |> Enum.sort_by(& &1.order)
+    end
+
+    test "move #4 to #5", %{items: items} do
+      assert [
+               %{name: "item-0", order: 0},
+               %{name: "item-1", order: 10},
+               %{name: "item-2", order: 20},
+               %{name: "item-3", order: 30},
+               %{name: "item-4", order: 40}
+             ] = Orderable.reorder(items, 4, 5) |> Enum.sort_by(& &1.order)
+    end
   end
 end
